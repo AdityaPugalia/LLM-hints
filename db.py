ad955b5dd89c1e504ae96ea279ada467386e4d5f
AdityaPugalia <adityapugalia30@gmail.com>
2025-03-15 18:50:04 +0800

    Added Parallelizability

--- db.py
+++ db.py
@@ -4,10 +4,12 @@ import time
 import os
 from typing import Tuple
 import config
+import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 class Database:
-    def __init__(self, host = 'localhost', port = 5432, dbname = None, user = 'postgres', password = None):
+    def __init__(self, host = 'localhost', port = config.PORT, dbname = None, user = 'postgres', password = None, query_timeout : int = 300):
         self.db_name = dbname
         self.db_user = user
         self.db_password = password
@@ -15,6 +17,9 @@ class Database:
         self.db_port = port
         self.conn = None
         self.cursor = None
+        self.monitor_thread = threading.Thread(target=self.monitor_long_queries, daemon=True)
+        self.monitor_thread_stop_event = threading.Event()
+        self.query_timeout = query_timeout
 
     def connect(self):
         """
@@ -28,6 +33,9 @@ class Database:
             port=self.db_port
         )
         self.cursor = self.conn.cursor()
+        self.monitor_thread_stop_event.clear()
+        self.monitor_thread.start()
+        
 
     def close(self):
         """
@@ -37,24 +45,27 @@ class Database:
             self.cursor.close()
         if self.conn:
             self.conn.close()
+        if self.monitor_thread is not None:
+            self.monitor_thread_stop_event.set()
+            self.monitor_thread.join()
 
-    def executeQuery(self, query, params=None):
+    def executeQuery(self, query, params=None, conn = None):
         """
         Execute a query and return the result
         """
         try:
+            cursor = conn.cursor() if conn is not None else self.conn.cursor()
             start_time = time.time()
-            self.cursor.execute(query, params)
+            cursor.execute(query, params)
+            execution_time = time.time() - start_time
 
-            if self.cursor.description:
-                result = self.cursor.fetchall()
-                row_count = self.cursor.rowcount
+            if cursor.description:
+                result = cursor.fetchall()
+                row_count = cursor.rowcount
             else:
                 self.conn.commit()
                 result = None
                 row_count = 0
-
-            execution_time = time.time() - start_time
             return result, execution_time, row_count, None
         except psycopg2.Error as e:
             print(f"Error executing query: {e}")
@@ -88,24 +99,77 @@ class Database:
             qep_rows = result[0][0][0]["Plan"]["Plan Rows"]
         return result[0][0], qep_cost, qep_rows, execution_time, error
     
-    def runExecutions(self, query) -> Tuple[float, Exception]:
+    def runExecutions(self, query: str, num_runs: int = 5) -> Tuple[float, Exception]:
+        """
+        Runs `num_runs` executions of the query in parallel and returns the average execution time.
+        """
         avg_time = 0
-        for i in range(5):
-            result, execution_time, _, error = self.executeQuery(query)
+        errors = []
+
+        # Function to execute query with a new connection
+        def run_query():
+            conn = psycopg2.connect(
+                dbname=self.db_name,
+                user=self.db_user,
+                password=self.db_password,
+                host=self.db_host,
+                port=self.db_port
+            )
+            result, execution_time, _, error = self.executeQuery(query, conn = conn)
+            conn.close()  # Ensure connection is closed
             if error:
-                return None, error
-            avg_time += execution_time
-        avg_time /= 5
+                errors.append(error)
+                return 0  # Return 0 execution time in case of error
+            return execution_time
+
+        # Execute queries in parallel
+        with ThreadPoolExecutor(max_workers=num_runs) as executor:
+            futures = [executor.submit(run_query) for _ in range(num_runs)]
+            
+            # Collect execution times
+            execution_times = [future.result() for future in as_completed(futures)]
+
+        # Compute average time
+        if errors:
+            return None, errors[0]  # Return the first error encountered
+
+        avg_time = sum(execution_times) / num_runs
         return avg_time, None
         
-    def compareQuery(self, query, alt_query):
-        query_avg, error = self.runExecutions(query)
+    def compareQuery(self, query, alt_query, num_runs : int = 1):
+        query_avg, error = self.runExecutions(query, num_runs)
         if error:
             return None, None, None, error
-        alt_query_avg, error = self.runExecutions(alt_query)
+        alt_query_avg, error = self.runExecutions(alt_query, num_runs)
         if error:
             return None, None, None, error
         return alt_query_avg < query_avg, query_avg, alt_query_avg, None
+    
+    def compare_n_queries(self, queries, num_queries : int = 1, num_runs : int = 1):
+        # Execute queries in parallel
+        with ThreadPoolExecutor(max_workers=num_queries) as executor:
+            futures = [executor.submit(self.runExecutions, queries[i], num_runs) for i in range(num_queries)]
+            
+            # Collect execution times
+            results = [future.result() for future in futures]
+        improved_list = []
+        alt_query_time_list = []
+        query_result = results.pop(0)
+        if query_result[1] is not None:
+            return None, None, None, query_result[1]
+        else:
+            query_avg = query_result[0]
+        for result in results:
+            if result[1] is not None:
+                improved_list.append(False)
+                alt_query_time_list.append(None)
+                continue
+            alt_query_time = result[0]
+            alt_query_time_list.append(alt_query_time)
+            improved = alt_query_time < query_avg
+            improved_list.append(improved)
+        
+        return (query_avg, improved_list, alt_query_time_list, None)
 
     def getStatistics(self, table_names, output_folder = 'data', output_file='table_statistics.json'):
         """Retrieve statistics for the specified tables and save them to a file."""
@@ -155,7 +219,8 @@ class Database:
             print(f"Error fetching statistics: {e}")
             raise
 
-    def get_config_settings(self, output_folder='data', output_file='config_settings.json'):
+
+    def get_config_settings(self, output_folder=config.DATA_FOLDER_PATH, output_file='config_settings.json'):
         """Fetch specified PostgreSQL configuration settings and save to a file."""
         if not self.conn or not self.cursor:
             raise Exception("Database connection is not established. Call connect() first.")
@@ -183,12 +248,22 @@ class Database:
                         "unit": result[2],
                         "context": result[3],
                     }
+                else:
+                    # If `hash_mem_multiplier` does not exist, set it to 1
+                    if param == "hash_mem_multiplier":
+                        settings[param] = {
+                            "name": param,
+                            "setting": "1",
+                            "unit": None,
+                            "context": "Manually Set",
+                        }
 
             # Write settings to a file in JSON format
             with open(output_path, 'w') as file:
                 json.dump(settings, file, indent=4)
 
             print(f"Configuration settings saved to {output_path}")
+
         except Exception as e:
             print(f"Error fetching configuration settings: {e}")
             raise
@@ -233,10 +308,110 @@ class Database:
         except Exception as e:
             print(f"Error extracting schema: {e}")
             raise
+    
+    def get_indexes(self, output_folder=config.DATA_FOLDER_PATH, output_file='database_indexes.json'):
+        """Retrieve all indexes for the database and save them to a JSON file."""
+        if not self.conn or not self.cursor:
+            raise Exception("Database connection is not established. Call connect() first.")
+
+        # Ensure the output folder exists
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Combine folder and file name
+        output_path = os.path.join(output_folder, output_file)
+
+        indexes = {}
+
+        try:
+            # Query to get indexes for all tables in the current database
+            query = """
+            SELECT
+                t.relname AS table_name,
+                i.relname AS index_name,
+                array_to_string(array_agg(a.attname), ', ') AS column_names,
+                ix.indisunique AS is_unique,
+                ix.indisprimary AS is_primary
+            FROM pg_index ix
+            JOIN pg_class t ON t.oid = ix.indrelid
+            JOIN pg_class i ON i.oid = ix.indexrelid
+            LEFT JOIN pg_attribute a ON a.attrelid = t.oid AND a.attnum = ANY(ix.indkey)
+            JOIN pg_namespace n ON n.oid = t.relnamespace
+            WHERE n.nspname = 'public'
+            GROUP BY t.relname, i.relname, ix.indisunique, ix.indisprimary
+            ORDER BY t.relname, i.relname;
+            """
+            self.cursor.execute(query)
+            results = self.cursor.fetchall()
+
+            # Organize data by table name
+            for table_name, index_name, column_names, is_unique, is_primary in results:
+                if table_name not in indexes:
+                    indexes[table_name] = []
+                indexes[table_name].append({
+                    "index_name": index_name,
+                    "columns": column_names,
+                    "is_unique": is_unique,
+                    "is_primary": is_primary
+                })
+
+            # Write indexes to a JSON file
+            with open(output_path, 'w') as file:
+                json.dump(indexes, file, indent=4)
+
+            print(f"Database indexes saved to {output_path}")
+
+        except Exception as e:
+            print(f"Error extracting indexes: {e}")
+            raise
+    
+    def monitor_long_queries(self):
+        """
+        Periodically checks for long-running queries and cancels them.
+        """
+        while not self.monitor_thread_stop_event.is_set():
+            try:
+                conn = psycopg2.connect(
+                dbname=self.db_name,
+                user=self.db_user,
+                password=self.db_password,
+                host=self.db_host,
+                port=self.db_port
+            )
+                conn.autocommit = True
+                cur = conn.cursor()
+
+                # Query to find long-running queries
+                cur.execute("""
+                    SELECT pid, age(now(), query_start), query
+                    FROM pg_stat_activity
+                    WHERE state = 'active'
+                    AND query_start < now() - interval '%s seconds';
+                """, (self.query_timeout,))
+
+                long_queries = cur.fetchall()
+
+                for pid, duration, query in long_queries:
+                    print(f"Cancelling query (PID: {pid}, Duration: {duration}): {query}")
+                    cur.execute(f"SELECT pg_cancel_backend({pid})")
+
+                cur.close()
+                conn.close()
+
+            except Exception as e:
+                print(f"Error monitoring queries: {e}")
+
+            # Check every 10 seconds
+            time.sleep(10)
+        print('exiting the monitor query thread')
+
 
 if __name__ == "__main__":
-    db = Database(user= config.USER, dbname= config.DBASE)
+    import pandas as pd
+    db = Database(user= config.USER, dbname= config.DBASE, port= config.PORT, password= config.PASSWORD)
     db.connect()
-    db.extract_schema()
+    queries = pd.read_csv(config.TPCH)['queries']
+    time.sleep(5)
+    results = db.compare_n_queries([queries[2], queries[1], queries[1], queries[1]], 4)
+    print(results)
     db.close()
 
--- zero_shot_pipeline.py
+++ zero_shot_pipeline.py
@@ -8,13 +8,16 @@ from vector_database import VectorDatabase
 import torch
 from embeddings import nodeEmbedder
 import ipynb
-from ipynb.fs.full.GNN_embeddings import GNNEncoder
 from torch_geometric.data import Data
+import json
+import logging
+from torch_geometric.nn import GCNConv, global_mean_pool
+import torch.nn.functional as F
 
 
 
 class ZeroShot:
-    def __init__(self, query_path :str, gnn_model_path: str, GNN_class : GNNEncoder, vector_db : VectorDatabase):
+    def __init__(self, query_path :str, gnn_model_path: str, GNN_class, vector_db : VectorDatabase):
         self.db = Database(user= config.USER, dbname= config.DBASE)
         self.queries = pd.read_csv(query_path)['queries']
         self.vector_db = vector_db
@@ -25,8 +28,13 @@ class ZeroShot:
             self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.node_embedder = nodeEmbedder(1, self.device, False)
         self.GNN_model = GNN_class
-        self.GNN_model.load_state_dict(torch.load(gnn_model_path))
+        self.GNN_model.load_state_dict(torch.load(gnn_model_path, map_location= self.device))
         self.GNN_model.to(self.device)
+        logging.basicConfig(    
+            filename=config.ZERO_SHOT_ERROR_LOG,  # Log file name
+            level=logging.ERROR,       # Log only errors and above
+            format="%(asctime)s - %(levelname)s - %(message)s",  # Log format
+        )
 
 
     def get_qep_graph(self, command : str):
@@ -36,31 +44,42 @@ class ZeroShot:
         g.parseQep(qep)
         return g, qep, error
 
-    def run_improvedqueryschedule(self, query: str):
+    def run_improvedqueryschedule(self, query: str, num_alts: int = 3):
         embeddings = None
         g, qep, error = self.get_qep_graph(query)
         if error:
-            print(f'the following error occired - {error} while parsing the query \n {query}')
+            print(f'the following error occured - {error} while parsing the query \n {query}')
             raise error
-        hints, alt_queries, error = self.get_alt_query(query, g.nodes, g.edges)
+        hints, alt_queries, error = self.get_alt_query(query, g.nodes, g.edges, 3)
         if error:
             print(f'the following error occured - {error} while getting alternate queries for the following query:\n {query}')
             raise error
-        for i in range(len(hints)):
-            improved, query_time, alt_query_time, error = self.db.compareQuery(query, alt_queries[i])
-            if error:
-                print(f'the following error occured - {error} while running the following alt query:\n {alt_queries[i]}')
-                continue
+        improved_count = 0
+        print('improving the query...')
+        queries = [query]
+        queries.extend(alt_queries)
+        query_time, improved_list, alt_query_time_list, error = self.db.compare_n_queries(queries, num_alts + 1)
+        if error:
+            print(f'the following error occured - {error} while parsing the query \n {query}')
+        for i in range(num_alts):
+            improved = improved_list[i]
+            alt_query_time = alt_query_time_list[i]
             if improved:
-                time_diff = query_time - alt_query_time
-                metadata = pd.Dataframe({'query': query, 'hints': hints[i], 'time_diff': time_diff})
-                if embeddings is not None:
+                improved_count += 1
+                time_improved = (query_time - alt_query_time) / query_time
+                config_settings = json.dumps(self.node_embedder.extractConfigurations(config.DATABASE_CONFIG))
+                metadata = pd.DataFrame({'query': [query], 'hints': [hints[i]], 'time_improved': [time_improved], 'database_config' : [config_settings]})
+                if embeddings is None:
                     embeddings = self.embed(query)
-                self.vector_db.add_vectors(embeddings = embeddings, metadata=metadata)
+                self.vector_db.add_vectors(embeddings = embeddings.detach().cpu().numpy(), metadata=metadata)
+        # Append to a simple text file
+        with open(config.ZERO_SHOT_STATUS, "a") as f:
+            f.write(f"Added {improved_count} alternative queries for the original query: {query}\n")
 
-    def get_alt_query(self, command, nodes, edges):
+
+    def get_alt_query(self, command, nodes, edges, num_alts):
         try:
-            hints, mod_queries = self.llm_model.get_n_hints(command, nodes, edges)
+            hints, mod_queries = self.llm_model.get_n_hints(command, nodes, edges, num_alts)
             return hints, mod_queries, None
         except Exception as e:
             print(f'An unexpected error occured {e}')
@@ -68,21 +87,38 @@ class ZeroShot:
     
     def embed(self, query):
         graph, _, _ = self.get_qep_graph(query)
-        query_embedding = torch.tensor(self.node_embedder.batch_processor(nodes=graph.nodes)[0], dtype=torch.float)
+        query_embedding = torch.tensor(self.node_embedder.batch_processor(data=[graph.nodes])[0], dtype=torch.float)
         query_tree = Data(query_embedding, torch.tensor(graph.edges, dtype=torch.long)).to(self.device)
-        query_embedding = self.GNN_model(query_tree)
+        query_tree.batch = torch.zeros(query_tree.x.shape[0], dtype=torch.long).to(self.device)
+        query_embedding = self.GNN_model(query_tree.x, query_tree.edge_index, query_tree.batch)
         return query_embedding
     
     def run(self):
-        try:
-            self.db.connect()
-            for query in self.queries:
+        self.db.connect()
+        for query in self.queries:
+            try:
                 self.run_improvedqueryschedule(query)
-        except Exception as e:
-            print(e)
-        finally:
-            self.db.close()
-        pass
+            except Exception as e:
+                logging.error(f"Error executing query: {query}", exc_info=True)  # Logs the error with traceback
+        self.db.close()
 
 if __name__ == '__main__':
-    pass
+    class GNNEncoder(torch.nn.Module):
+        def __init__(self, input_dim, hidden_dim, output_dim):
+            super(GNNEncoder, self).__init__()
+            self.conv1 = GCNConv(input_dim, hidden_dim)
+            self.conv2 = GCNConv(hidden_dim, output_dim)
+
+        def forward(self, x, edge_index, batch):
+            x = self.conv1(x, edge_index)
+            x = F.relu(x)
+            x = self.conv2(x, edge_index)
+            # Global mean pooling to get a graph-level representation
+            x = global_mean_pool(x, batch)
+            return x
+    
+    GNN = GNNEncoder(48, 64, 32)
+
+    vector_db = VectorDatabase(config.VD_INDEX, 32, 'cosine', config.METADATA, ['query', 'hints', 'time_improved', 'database_config'], None) 
+    zero_shot_pipeline = ZeroShot(config.TPCH, config.GNN_MODEL, GNN, vector_db)
+    zero_shot_pipeline.run()
